1dea20ae2c28c23c68a2c89a740a6f7fc66efbe1
Pell35 <pjelliott11@gmail.com>
2025-02-23 23:58:15 -0600

    Learned how to use the range function within the for loop

--- sum_expenses.py
+++ sum_expenses.py
@@ -1,6 +1,21 @@
-expenses = [10.50, 8, 5, 15, 20, 5, 3]
+# expenses = [10.50, 8, 5, 15, 20, 5, 3]
+
+# total = sum(expenses)
+
+
+# print("You spent $", total, " on lunch this week.", sep = "")
+
+total = 0
+
+expenses = []
+
+num_expenses = int(input("Enter # of expenses:"))
+
+for i in range(num_expenses):
+    expenses.append(float(input("Enter an expense:")))
 
 total = sum(expenses)
 
+print("You spent $", total, sep= "")
 
-print("You spent $", total, " on lunch this week.", sep = "")
+# Learned how to use range in my for loop
