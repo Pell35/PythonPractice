d217e3505f0f3f9ccb1bd8ecec3e887e49d9f2e3
Pell35 <pjelliott11@gmail.com>
2025-02-25 23:58:56 -0600

    Created a tip calculator to practice

---
+++ tipCalculator.py
@@ -0,0 +1,10 @@
+cost = input("What was the price of your meal? \n")
+
+percent = input("What percentage would you like to tip? 10,15,20,25 \n")
+
+decimal = int(percent) / 100
+
+tip = float(cost)*decimal
+
+total = float(cost)+tip
+print("To tip " + percent +"% Your tip will be: $" + str(tip) + "Your total cost will be : $" + str(total))
