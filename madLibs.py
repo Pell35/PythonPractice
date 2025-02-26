84ed14827277d77f5afc4877615c87ff23c4750a
Pell35 <pjelliott11@gmail.com>
2025-02-26 00:13:22 -0600

    Created a basic madLib generator as practice

---
+++ madLibs.py
@@ -0,0 +1,17 @@
+place = input("Enter a place. \n")
+adjective1 = input("Enter an adjective. \n")
+animal =  input("Enter an animal. \n")
+verb1 = input("Enter a verb that ends in ing. \n")
+object = input("Enter an object. \n")
+verb2 = input("Enter another verb. \n")
+adjective2 = input("Enter another adjective. \n")
+name = input("Enter a name of a person or character. \n")
+exclamation = input("Enter a word of exclamation. \n")
+adjective3 = input("Enter another adjective. \n")
+
+
+
+print("Today, I went to the " + str(place) + " and saw a " + str(adjective1) + " " + str(animal) + 
+      ". It was " + str(verb1) + " all over the " + str(object) +
+      "! I decided to " + str(verb2) + ", but then a " + str(adjective2)+ " " + str(name) + " appeared and shouted, " +
+      str(exclamation) + "! It was the most " + str(adjective3) + " day ever!")
