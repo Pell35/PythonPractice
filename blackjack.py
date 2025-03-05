00ec826608cd4f5a5607712bd6dcb0ae5f43432c
Pell35 <pjelliott11@gmail.com>
2025-03-04 22:58:58 -0600

    Updated my input statement, and my card values so that it would run correctly. I still need to update conditionals for the dealers cards

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,8 @@
 import random
 
 card_values = {
-    "A": [1, 11], 
+    "A": 1,
+    "A2": 11, 
     "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, 
     "7": 7, "8": 8, "9": 9, "10": 10, 
     "J": 10, "Q": 10, "K": 10
@@ -11,9 +12,9 @@ random_card1 = random.choice(list(card_values.keys()))
 
 random_card2 = random.choice(list(card_values.keys()))
 
-random_card1_val = random_card1.values()
+random_card1_val = card_values[random_card1]  
 
-random_card2_val =  random_card2.values()
+random_card2_val =  card_values[random_card2]  
 
 player_card_val = random_card1_val + random_card2_val
 
@@ -21,18 +22,18 @@ dealer_card1 = random.choice(list(card_values.keys()))
 
 dealer_card2 = random.choice(list(card_values.keys()))
 
-dealer_card1_val = dealer_card1.values()
+dealer_card1_val = card_values[dealer_card1 ]  
 
-dealer_card2_val = dealer_card2.values()
+dealer_card2_val = card_values[dealer_card2]  
 
-dealer_card_val = dealer_card1_val + dealer_card2_val
+dealer_card_val = int(dealer_card1_val) + int(dealer_card2_val)
 
 
-answer = print(input("You were dealt: " + str(random_card1) + " & " + str(random_card2) + ". \nThere value is: " +  str(player_card_val) + ". \n Dealer card value is: " + str(dealer_card_val) + ". \n Would you like to hold or hit? "))
+answer = input("You were dealt: " + str(random_card1) + " & " + str(random_card2) + ". \nThere value is: " +  str(player_card_val) + ". \n Dealer card value is: " + str(dealer_card_val) + ". \n Would you like to hold or hit? ")
 
 if answer == "hit":
     random_card3 = random.choice(list(card_values.keys()))
-    random_card3_val = random_card3.values()
+    random_card3_val = card_values[random_card3]  
     player_card_val = random_card1_val + random_card2_val + random_card3_val
     if player_card_val >dealer_card_val and player_card_val <=21:
         print("You have: "+ str(player_card_val) + ", you win!")
