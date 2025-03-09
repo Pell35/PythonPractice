2197d3246ece9899517755feab672c8271740930
Pell35 <pjelliott11@gmail.com>
2025-03-08 22:58:39 -0600

    Updated it to be less code and more concise

--- vowelCounter.py
+++ vowelCounter.py
@@ -1,13 +1,11 @@
 vowels = ["a", "e", "i", "o", "u"]
 
-word = input("Give a word. \n")
-
-ranNum = len(word)
+word = input("Give a word. \n").lower()
 
 numVowels = 0
 
-for i in range(ranNum):
-    if word[i] == "a" or word[i] == "e" or word[i] == "i" or word[i] == "o" or word[i] == "u":
+for letter in word:
+    if letter in vowels:
         numVowels += 1
 
 print("Your word, " + str(word) + ", has " + str(numVowels) + " vowels.")
