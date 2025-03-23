28d80ae76c2c8e056b341c65a00b617c10f58a93
Pell35 <pjelliott11@gmail.com>
2025-03-22 23:26:02 -0500

    Updated conditionals to check for the day user was born, and determine age

--- ageCalculator1.py
+++ ageCalculator1.py
@@ -6,12 +6,24 @@ current_year = datetime.datetime.now().year
 
 birthMonth =  int(input("What month were you born? (1-12) \n"))
 current_month = datetime.datetime.now().month
-if current_month < birthMonth:
-    
+
+birthDay = int(input("What day were you born? (1-31) \n"))
+current_day = datetime.datetime.now().day
+
+if current_month == birthMonth and current_day == birthDay:   
+    age = current_year - birthYear
+    print("You are " + str(age) +" years old. Happy Birthday! ")
+elif current_month == birthMonth and current_day < birthDay:
     age = current_year - birthYear-1
+    print("You are " + str(age) +" years old.")    
+elif current_month == birthMonth and current_day > birthDay:
+    age = current_year - birthYear
+    print("You are " + str(age) +" years old.")    
+elif current_month<birthMonth:
+    age = current_year - birthYear-1
+    print("You are " + str(age) +" years old.")        
 else:
     age = current_year - birthYear
- 
+    print("You are " + str(age) +" years old.")
 
 
-print("You are " + str(age) +" years old.")
